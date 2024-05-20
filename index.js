14b9f9e366f65c93e09fe963da9cbb06062db8c1
Deetsy <amardeepdeepak2016@gmail.com>
2024-05-20 12:21:21 +0530

    add prepend function

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+//Node factory
+
 function node(value) {
   return {
     value: value,
     nextNode: null,
   };
 }
+// factory linkedList
 function linkedList() {
   let headNode = {
     value: null,
     nextNode: null,
   };
+  //function append
   function append(value) {
     newNode = node(value);
-
     function check(node) {
       if (node.nextNode == null) {
         node.nextNode = newNode;
@@ -21,9 +24,17 @@ function linkedList() {
     }
     check(headNode);
   }
-
+  //function prepend
+  function prepend(value) {
+    newNode = node(value);
+    secondNode = headNode.nextNode;
+    headNode.nextNode = newNode;
+    newNode.nextNode = secondNode;
+  }
+  // values to return
   return {
     headNode,
     append,
+    prepend,
   };
 }
