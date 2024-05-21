ce47e94bad747e4f90d1b5d349842e05bb49cd51
Deetsy <amardeepdeepak2016@gmail.com>
2024-05-21 21:51:13 +0530

    made some more functions

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function linkedList() {
     value: null,
     nextNode: null,
   };
-  //function append
+  //1 function append
+
   function append(value) {
     newNode = node(value);
     function check(node) {
@@ -24,17 +25,51 @@ function linkedList() {
     }
     check(headNode);
   }
-  //function prepend
+  //2 function prepend
+
   function prepend(value) {
     newNode = node(value);
     secondNode = headNode.nextNode;
     headNode.nextNode = newNode;
     newNode.nextNode = secondNode;
   }
+  // 3 checkSize to get number of nodes
+  function checkSize(node) {
+    let length = 0;
+    if (node.nextNode == null) {
+      return length;
+    } else {
+      length++;
+      checkSize(node.nextNode);
+    }
+    return length;
+  }
+  function size() {
+    return checkSize(headNode);
+  }
+  //4 head returns first node after headNode
+  function head() {
+    return headNode.nextNode;
+  }
+  //5 tail returns last node
+  function tail() {
+    let tail = {};
+    function returnTail(node) {
+      if (node.nextNode == null) {
+        return node;
+      } else {
+        returnTail(node.nextNode);
+      }
+    }
+    return (tail = returnTail(headNode));
+  }
   // values to return
   return {
+    size,
     headNode,
     append,
     prepend,
+    head,
+    tail,
   };
 }
